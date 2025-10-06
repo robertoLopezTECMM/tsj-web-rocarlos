ac8b1242b21dacd7a98606a0e4ea85caa26f5c3c
robertoLopezTECMM <roberto.karlos.lopez@gmail.com>
2025-10-06 09:36:54 -0600

    TSJ-WEB-20: feat: new preview for student credential created

--- src/components/credencial/index.tsx
+++ src/components/credencial/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./index.css";
 
-import { CredencialType } from "../../types/componentsTypes";
+import { CredencialStudentType, CredencialType } from "../../types/componentsTypes";
 import qrTemplate from "../../assets/images/qrTemplate.png";
 
 export const Credencial = ({
@@ -33,6 +33,64 @@ export const Credencial = ({
 
             <p className="component-credencialSmallData">
               Número de nomina: {noEmpleado}
+            </p>
+
+            
+
+            <div className="component-credencialSmallDataRow">
+                <p className="component-credencialSmallData">NSS: {noSeguro}</p>
+                <p className="component-credencialSmallData"> Tipo de sangre: {tipoSangre} </p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="component-credencialContainerBack">
+        <div className="component-credencialContainerDatosBack">
+          <p className="component-credencialUnidadAcademica">
+            UNIDAD ACADÉMICA {unidadAcademica}
+          </p>
+
+          <div className="component-credencialQrContainer">
+            <img className="component-credencialPhoto" src={qrTemplate} />
+          </div>
+
+          <p className="component-credencialPhone">CONTACTO DE EMERGENCIA</p>
+          <p className="component-credencialPhone">{telefonoEmergencia}</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export const CredencialAlumno = ({
+  photoUrl,
+  nombre,
+  carrera,
+  rol,
+  noControl,
+  noSeguro,
+  tipoSangre,
+  telefonoEmergencia,
+  unidadAcademica,
+}: CredencialStudentType) => {
+  return (
+    <div className="component-credencialContainerViews">
+      <div className="component-credencialContainerFront">
+        <div className="component-credencialContainerDatos">
+          <div className="component-credencialPhotoContainer">
+            <img src={photoUrl} />
+          </div>
+
+          <p className="component-credencialName">{nombre}</p>
+          <p className="component-credencialCarrera">{carrera}</p>
+          <p className="component-credencialPosicion">{rol}</p>
+
+          <div className="component-credencialOtherData">
+
+            <p className="component-credencialSmallData">
+              Número de control: {noControl}
             </p>
 
             
