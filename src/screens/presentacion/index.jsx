6c70e7a4e3340eb565348788369d6f4ef91a7a4d
robertoLopezTECMM <roberto.karlos.lopez@gmail.com>
2025-10-10 13:13:01 -0600

    TSJ-feat: mapJaliscoPresentation component created

--- src/screens/presentacion/index.jsx
+++ src/screens/presentacion/index.jsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import MapJalisco from '../../components/jaliscoMap'
+import React, { useState } from 'react'
 import './index.css'
 import { BasicContainer } from '../../components/basicContainer'
+import MapJalisco from '../../components/jaliscoMap'
+import Box from '@mui/material/Box'
+import Modal from '@mui/material/Modal';
+import MapJaliscoPresentation from '../../components/jaliscoMapPresentation'
 
 export const Presentacion = () => {
+    const style = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: '80dvw',
+    height:'65dvh',
+
+
+    boxShadow: 24,
+    p: 4,
+    };
+
+
+      const [open, setOpen] = useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   return (
-    <BasicContainer >
-        <div className='mapaPresentacion'>
-            <MapJalisco/>
-        </div>
-    </BasicContainer>
+    <BasicContainer>
+      <div className='mapaPresentacion'>
+          <MapJaliscoPresentation/>
+      </div>
+    </BasicContainer >
+
   )
 }
